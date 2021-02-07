7c2041ebf887047ceb3dc355efcb688987b087ef
flign <68303348+flign@users.noreply.github.com>
2021-02-07 16:06:10 +0200

    Add project files.

--- Data_Tree_Depth/DataTreeService.cs
+++ Data_Tree_Depth/DataTreeService.cs
@@ -4,7 +4,7 @@ namespace Data_Tree_Depth
 {
     public class DataTreeService
     {
-        private readonly BranchService nodeService = new BranchService();
+        private readonly BranchService branchService = new BranchService();
         public Branch CreateDataTree()
         {
             /* Creating tree below:
@@ -18,22 +18,22 @@ namespace Data_Tree_Depth
             *                |
             *                K
             */
-            Branch tree = nodeService.NewBranch('A');
-            tree.branch.Add(nodeService.NewBranch('B'));
-            tree.branch.Add(nodeService.NewBranch('C'));
-            tree.branch[0].branch.Add(nodeService.NewBranch('D'));
-            tree.branch[1].branch.Add(nodeService.NewBranch('E'));
-            tree.branch[1].branch.Add(nodeService.NewBranch('F'));
-            tree.branch[1].branch.Add(nodeService.NewBranch('G'));
-            tree.branch[1].branch[0].branch.Add(nodeService.NewBranch('H'));
-            tree.branch[1].branch[1].branch.Add(nodeService.NewBranch('I'));
-            tree.branch[1].branch[1].branch.Add(nodeService.NewBranch('J'));
-            tree.branch[1].branch[1].branch[0].branch.Add(nodeService.NewBranch('K'));
+            Branch tree = branchService.NewBranch('A');
+            tree.branch.Add(branchService.NewBranch('B'));
+            tree.branch.Add(branchService.NewBranch('C'));
+            tree.branch[0].branch.Add(branchService.NewBranch('D'));
+            tree.branch[1].branch.Add(branchService.NewBranch('E'));
+            tree.branch[1].branch.Add(branchService.NewBranch('F'));
+            tree.branch[1].branch.Add(branchService.NewBranch('G'));
+            tree.branch[1].branch[0].branch.Add(branchService.NewBranch('H'));
+            tree.branch[1].branch[1].branch.Add(branchService.NewBranch('I'));
+            tree.branch[1].branch[1].branch.Add(branchService.NewBranch('J'));
+            tree.branch[1].branch[1].branch[0].branch.Add(branchService.NewBranch('K'));
             return tree;
         }
         public void PrintDepth(Branch tree)
         {
-            Console.Write("Giliausias lygmuo: " + nodeService.DepthOfTree(tree) + "\n");
+            Console.Write("Giliausias lygmuo: " + branchService.DepthOfTree(tree) + "\n");
         }
     }
 }
